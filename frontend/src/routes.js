c75e529d8496f7d2c5c48e4635fa076e0b3a48a3
Worldspawn <worldspawn.web@gmail.com>
2024-01-05 17:16:21 +0300

    feat: routes

---
+++ frontend/src/routes.js
@@ -0,0 +1,10 @@
+const apiPath = '/api/v1';
+
+export default {
+  login: () => `${apiPath}/login`,
+  signup: () => `${apiPath}/signup`,
+  data: () => `${apiPath}/data`,
+  loginPage: () => '/login',
+  mainPage: () => '/',
+  signUpPage: () => '/signup',
+};
