223eeb1331d4ddc42f671d92f3fba4689efb042c
Worldspawn <worldspawn.web@gmail.com>
2024-01-05 17:18:08 +0300

    feat: auth provider

---
+++ frontend/src/providers/AuthProvider.jsx
@@ -0,0 +1,63 @@
+import React, { useMemo, useState, createContext, useContext } from 'react';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import routes from '../routes.js';
+import { actions as loadingStatusActions } from '../slices/loadingStatusSlice.js';
+
+const AuthContext = createContext({});
+const useAuth = () => useContext(AuthContext);
+
+const AuthProvider = ({ children }) => {
+  const dispatch = useDispatch();
+
+  const currentUser = JSON.parse(localStorage.getItem('user'));
+  const [user, setUser] = useState(currentUser);
+
+  const context = useMemo(() => {
+    const logIn = async (userData) => {
+      const response = await axios.post(routes.login(), userData);
+      const { data } = response;
+      localStorage.setItem('user', JSON.stringify(data));
+      setUser(data);
+    };
+
+    const signUp = async (username, password) => {
+      const response = await axios.post(routes.signup(), {
+        username,
+        password,
+      });
+      const { data } = response;
+      localStorage.setItem('user', JSON.stringify(data));
+      setUser(data);
+    };
+
+    const logOut = () => {
+      localStorage.removeItem('user');
+      dispatch(loadingStatusActions.unload());
+      setUser(null);
+    };
+
+    const getUserName = () => (user?.username ? user.username : null);
+
+    const getAuthHeader = () =>
+      user?.token ? { Authorization: `Bearer ${user.token}` } : {};
+
+    const loggedIn = !!user;
+
+    return {
+      logIn,
+      logOut,
+      signUp,
+      loggedIn,
+      getUserName,
+      getAuthHeader,
+    };
+  }, [dispatch, user]);
+
+  return (
+    <AuthContext.Provider value={context}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
+export { AuthContext, useAuth };
