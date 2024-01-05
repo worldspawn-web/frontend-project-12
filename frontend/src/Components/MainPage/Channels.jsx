ce6602641940eafdb3c4c8ad45702b114a5c67c1
Worldspawn <worldspawn.web@gmail.com>
2024-01-05 17:21:29 +0300

    feat: main page & channels

---
+++ frontend/src/Components/MainPage/Channels.jsx
@@ -0,0 +1,162 @@
+import { useDispatch } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+import LeoProfanity from 'leo-profanity';
+
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+import { actions as channelsActions } from '../../slices/channelsSlice';
+import * as modalSlice from '../../slices/modalSlice';
+
+const NotRemovableChannel = ({ name, id, isCurrentChannel, handleSelect }) => {
+  const { t } = useTranslation();
+
+  return (
+    <Button
+      onClick={() => handleSelect(Number(id))}
+      variant={isCurrentChannel(id) ? 'primary' : 'ligth'}
+      className="w-100 rounded-0 text-start"
+    >
+      <span className="me-1">{t('channels.channelSign')}</span>
+      {name}
+    </Button>
+  );
+};
+
+const RemovableChannel = ({
+  name,
+  id,
+  isCurrentChannel,
+  handleSelect,
+  handleRemove,
+  handleRename,
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <Dropdown as={ButtonGroup} className="d-flex">
+      <NotRemovableChannel
+        name={name}
+        id={id}
+        isCurrentChannel={isCurrentChannel}
+        handleSelect={handleSelect}
+      />
+      <Dropdown.Toggle
+        split
+        variant={isCurrentChannel(id) ? 'primary' : 'ligth'}
+        id="dropdown-split-basic"
+      >
+        <span className="visually-hidden">{t('channels.control')}</span>
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={handleRemove(Number(id))}>
+          {t('channels.remove')}
+        </Dropdown.Item>
+        <Dropdown.Item onClick={handleRename(Number(id), name)}>
+          {t('channels.rename')}
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
+
+const Channels = ({ channels, currentChannel }) => {
+  const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const profanityFilter = LeoProfanity;
+
+  const isCurrentChannel = (currentId) => {
+    const { id } = currentChannel;
+    return currentId === id;
+  };
+
+  const handleSelect = (id) => {
+    dispatch(channelsActions.setCurrentChannel(id));
+  };
+
+  const handleAdd = () => {
+    dispatch(modalSlice.actions.open({ type: 'add' }));
+  };
+
+  const handleRename = (id, name) => () => {
+    const context = {
+      channelId: id,
+      channelName: name,
+    };
+
+    dispatch(modalSlice.actions.open({ type: 'rename', context }));
+  };
+
+  const handleRemove = (id) => () => {
+    const context = {
+      channelId: id,
+    };
+
+    dispatch(modalSlice.actions.open({ type: 'remove', context }));
+  };
+
+  return (
+    <Col
+      xs={4}
+      md={2}
+      className="border-end px-0 bg-light flex-column h-100 d-flex"
+    >
+      <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
+        <b>{t('channels.title')}</b>
+        <Button
+          onClick={handleAdd}
+          variant="outline-primary"
+          className="p-0 d-flex btn-group-vertical"
+        >
+          <svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            width={20}
+            height={20}
+          >
+            <rect x="9.5" y="5" width="1" height="10" />
+            <rect x="5" y="9.5" width="10" height="1" />
+          </svg>
+          <span className="visually-hidden">+</span>
+        </Button>
+      </div>
+      <ul
+        id="channels-box"
+        className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block"
+      >
+        {channels.map(({ name, id, removable }) => {
+          if (removable) {
+            return (
+              <li key={id} className="nav-item w-100">
+                <RemovableChannel
+                  name={profanityFilter.clean(name)}
+                  id={id}
+                  isCurrentChannel={isCurrentChannel}
+                  handleSelect={handleSelect}
+                  handleRemove={handleRemove}
+                  handleRename={handleRename}
+                />
+              </li>
+            );
+          }
+          return (
+            <li key={id} className="nav-item w-100">
+              <NotRemovableChannel
+                name={profanityFilter.clean(name)}
+                id={id}
+                isCurrentChannel={isCurrentChannel}
+                handleSelect={handleSelect}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </Col>
+  );
+};
+
+export default Channels;
---
+++ frontend/src/Components/MainPage/MainPage.jsx
@@ -0,0 +1,101 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import Spinner from 'react-bootstrap/Spinner';
+import Button from 'react-bootstrap/Button';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { channelsSelectors } from '../../slices/channelsSlice';
+import { messagesSelectors } from '../../slices/messagesSlice';
+import { loadingStatusSelectors } from '../../slices/loadingStatusSlice';
+import { useServer } from '../../providers/ServerProvider';
+import fetchInitialData from '../../slices/thunk';
+import routes from '../../routes';
+import Channels from './Channels';
+import Messages from './Messages';
+import Modal from '../modals/index';
+
+const Error = () => {
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  return (
+    <Card className="text-center">
+      <Card.Body>
+        <Card.Title>{t('errors.smthWentWrong')}</Card.Title>
+        <Button onClick={() => navigate(routes.loginPage())}>
+          {t('buttons.reloadBtn')}
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+const LoadingState = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="m-auto w-auto">
+      <Spinner animation="border" variant="primary" role="status">
+        <span className="visually-hidden">{t('loadingStatus')}</span>
+      </Spinner>
+    </div>
+  );
+};
+
+const Children = () => {
+  const loadingStatus = useSelector(loadingStatusSelectors.getStatus);
+  const channels = useSelector(channelsSelectors.selectAllChannels);
+  const currentChannel = useSelector(channelsSelectors.selectCurrentChannel);
+  const currentChannelMessages = useSelector(
+    messagesSelectors.selectCurrentChannelMessages
+  );
+
+  switch (loadingStatus) {
+    case 'successful':
+      return (
+        <>
+          <Channels channels={channels} currentChannel={currentChannel} />
+          <Messages
+            channel={currentChannel}
+            messages={currentChannelMessages}
+          />
+        </>
+      );
+
+    case 'failed':
+      return <Error />;
+
+    default:
+      return <LoadingState />;
+  }
+};
+
+const MainPage = () => {
+  const dispatch = useDispatch();
+  const { getServerData, connectSocket, disconnectSocket } = useServer();
+
+  useEffect(() => {
+    dispatch(fetchInitialData(getServerData));
+    connectSocket();
+
+    return () => disconnectSocket();
+  }, [dispatch, connectSocket, disconnectSocket, getServerData]);
+
+  return (
+    <>
+      <Container className="h-100 my-4 overflow-hidden rounded shadow">
+        <Row className="h-100 bg-white flex-nowrap">
+          <Children />
+        </Row>
+      </Container>
+      <Modal />
+    </>
+  );
+};
+
+export default MainPage;
